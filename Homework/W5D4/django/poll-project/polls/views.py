0b716f1e5f1761de70aaeeaebe5ea0aeab60a7aa
byunghyunpark <openmind8735@gmail.com>
2016-10-06 23:35:45 +0900

    code review

---
+++ Homework/W5D4/django/poll-project/polls/views.py
@@ -0,0 +1,35 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Choice, Question
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'detail.html', {'question': question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'results.html', {'question': question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        choice_id = request.POST['choice']
+        selected_choice = question.choice_set.get(pk=choice_id)
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'detail.html', {
+            'question': question,
+            'error_message': "you didn't select a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        redirect_url = reverse('polls:results', args=(question.id,))
+        return HttpResponseRedirect(redirect_url)
